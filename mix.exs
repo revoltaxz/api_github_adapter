91d50ca128b739d42de92581cfe011860438da0d
Guilherme Revolta <guilhermerevolta@gmail.com>
2021-11-16 14:24:23 -0300

    Install Mox

--- mix.exs
+++ mix.exs
@@ -40,7 +40,8 @@ defmodule ApiGithubAdapter.MixProject do
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:httpoison, "~> 1.8"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:mox, "~> 1.0", only: :test}
     ]
   end
 
